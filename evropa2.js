407999ee536e85039598c849028255e4f178d76d
Olena Horovenko <elena.gorovenko@gmail.com>
2024-04-13 11:18:56 +0200

    initial commit

---
+++ evropa2.js
@@ -0,0 +1,5 @@
+import { radio } from "./radio.js";
+
+document.querySelector('#text').innerHTML = `
+<h1>${radio[0].name}</h1>
+`
---
+++ radio.js
@@ -0,0 +1,23 @@
+export const radio = [
+  {
+    name: 'Evropa 2',
+    frequency: '88.6 FM',
+    description:
+      'Evropa 2 je nejposlouchanější česká komerční rádio mezi mladými posluchači. Vysílá hudbu z žebříčků, kterou si sami vybírají posluchači a nejnovější hity.',
+    link: '/evropa2.html',
+  },
+  {
+    name: 'Frekvence 1',
+    frequency: '102.1 FM',
+    description:
+      'Frekvence 1 je česká soukromá rozhlasová stanice, která vysílá od roku 1991. Vysílá hudbu z žebříčků, kterou si sami vybírají posluchači a nejnovější hity.',
+    link: '/frekvence1.html',
+  },
+  {
+    name: 'Radiožurnál',
+    frequency: '92.6 FM',
+    description:
+      'Radiožurnál je česká rozhlasová stanice Českého rozhlasu. Vysílá hudbu z žebříčků, kterou si sami vybírají posluchači a nejnovější hity.',
+    link: '/radiozurnal.html',
+  },
+];
